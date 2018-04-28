bf74cd5c59c9996e7a1947727bc46be04975bf9b
Serban Costandache <serbanc94@gmail.com>
2018-04-28 12:04:03 +0300

    deleted test counter

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
 from app import app
 from flask import render_template
-from app.counter import count
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username':'test'}
-    test_string = count.bar()
-    return render_template('index.html', title = 'Student admission', user=user, teststr = test_string)
+    return render_template('index.html', title = 'Student admission', user=user)
